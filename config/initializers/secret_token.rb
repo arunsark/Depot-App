d1ec8c493ec03fe5af3a77ea8e6ec67a3c418b0e
Arun V <arun.vydianathan@gmail.com>
2011-03-22 18:42:38 +0530

    Initial Commit Depot App

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Depot::Application.config.secret_token = '0ef5a5e17831182e4cf45a55131b1b6367a3d0bcdf6f63cc22eb2693ec8d0c66bc778bbea8a32c519f9969a160fcfebd1fbe2a5ce13bfdd9b3d62cfa86a89fa1'
---
+++ test/unit/product_test.rb
@@ -0,0 +1,80 @@
+require 'test_helper'
+
+class ProductTest < ActiveSupport::TestCase
+  fixtures :products
+  
+  # Replace this with your real tests.
+  test "product attributes cannot be empty" do
+    product = Product.new
+    assert product.invalid?
+    assert product.errors[:title].any?
+    assert product.errors[:description].any?
+    assert product.errors[:price].any?
+    assert product.errors[:image_url].any?
+  end
+
+  test "product price must be postive" do
+    product = Product.new(:title=>"My Title",
+                          :description => "desc desc",
+                          :image_url => "desc.jpg")
+    product.price = -1
+    assert product.invalid?
+    assert_equal "must be greater than or equal to 0.01",
+    product.errors[:price].join(' ')
+
+    product.price = 0
+    assert product.invalid?
+    assert_equal "must be greater than or equal to 0.01",
+    product.errors[:price].join('; ')
+
+    product.price = 1
+    assert product.valid?
+  end
+
+  test "image url" do
+    ok = %w{ fred.jpg fred.gif fred.png Fred.JPG Fred.Png
+http://a.com/ab/ab/fred.gif}
+    
+    bad = %w{ fred.doc fred.gif/more fred.png.more Fred.JP }
+
+    ok.each do |name|
+      assert new_product(name).valid?,  "#{name} shouldn't be invalid"
+    end
+
+    bad.each do |name|
+      assert new_product(name).invalid?, "#{name} shouldn't be valid"
+    end
+  end
+
+  test "product is not valid without unique title" do
+
+    product = Product.new(:title => products(:ruby).title,
+                          :description => "xxxx",
+                          :image_url => "ruby.gif",
+                          :price => 49)
+    assert product.invalid?
+    assert !product.save
+    assert_equal "has already been taken", product.errors[:title].join('; ')
+  end
+
+
+   test "product is not valid without unique title - i18n" do
+
+    product = Product.new(:title => products(:ruby).title,
+                          :description => "xxxx",
+                          :image_url => "ruby.gif",
+                          :price => 49)
+    assert product.invalid?
+    assert !product.save
+    assert_equal I18n.translate('activerecord.errors.messages.taken'),product.errors[:title].join('; ')
+  end
+
+
+  def new_product(image_url)
+    Product.new(:title=>"Title",
+                :description => "Desc Desc",
+                :image_url => image_url,
+                :price => 10.95
+                )
+  end
+end
